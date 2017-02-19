a34b0fdc2d73345ebf3ed46d267242ba889e065a
Nasruddin <nasruddin.java@gmail.com>
2017-02-19 22:03:45 +0530

    Initial commit

---
+++ src/main/java/com/nasir/SimpleConsumer.java
@@ -0,0 +1,42 @@
+package com.nasir;
+
+/**
+ * Created by nasir on 19/2/17.
+ */
+import java.util.Properties;
+import java.util.Arrays;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class SimpleConsumer {
+    public static void main(String[] args) throws Exception {
+        String topicName = "HelloKafka1";
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer
+                <String, String>(props);
+        consumer.subscribe(Arrays.asList(topicName));
+
+        System.out.println("Subscribed to topic " + topicName);
+        int i = 0;
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+
+                System.out.printf("offset = %d, key = %s, value = %s\n",
+                        record.offset(), record.key(), record.value());
+        }
+    }
+}
+
